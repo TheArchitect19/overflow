9190df34c3f4ab9714315709d23fa9e16f162f46
Udit-UD <udgupta33@gmail.com>
2025-01-12 14:24:28 +0530

    fix: homepage ui for web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { motion } from "framer-motion";
 import { HeroHighlight } from "../components/ui/hero-highlight";
 import Herotext from "@/components/home/hero_section1";
 import { Hero } from "@/components/home/hero_section2";
@@ -9,12 +8,15 @@ export default function Home() {
     <div>
       {/* Background Content */}
       <HeroHighlight>
-        <motion.h1>
-          {/* Add your hero highlight content here */}
-          <div className=" w-[50%] h-full items-start ">
-            <Herotext />
-          </div>
-        </motion.h1>
+        {/* Add your hero highlight content here */}
+        <div
+          className=" w-4/5 flex items-center justify-start
+         md:px-6 lg:px-10"
+        >
+          <Herotext />
+
+          {/* TODO: Add HeroImage */}
+        </div>
       </HeroHighlight>
     </div>
   );
--- apps/web/components/home/hero_section1.tsx
+++ apps/web/components/home/hero_section1.tsx
@@ -1,25 +1,23 @@
 import { Button } from "../ui/button";
 import { Cover } from "../ui/cover";
+import { motion } from "framer-motion";
 
 export default function Herotext() {
-    return (
-        <>
-            <div>
-                <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-                    Unlock your dream<br /><Cover>Tech Job</Cover>
-                </h1>
-            </div>
-            <div>
-                Use Gen AI driven ATS algorithm and give Mock Interviews from industry experts.
-            </div>
-            <div>
-                <Button>
-                    ATS
-                </Button>
-                <Button>
-                    Mock Interview
-                </Button>
-            </div>
-        </>
-    )
+  return (
+    <div className=" flex flex-col gap-4 justify-between items-start">
+      <motion.h1 className=" w-3/5 text-4xl md:text-4xl lg:text-6xl font-semibold  relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        Unlock your dream
+        <br />
+        <Cover>Tech Job</Cover>
+      </motion.h1>
+      <div>
+        Use Gen AI driven ATS algorithm and give <br /> Mock Interviews from
+        industry experts.
+      </div>
+      <div className="flex gap-4 items-center">
+        <Button>ATS</Button>
+        <Button>Mock Interview</Button>
+      </div>
+    </div>
+  );
 }
